204282da543fa38de78d5a1ba07c436e93778e87
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-07-22 23:38:41 +0100

    Modified some files

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -17,34 +17,29 @@ module.exports = {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
         type: 'asset/resource',
         use: [
+          'file-loader',
           {
             loader: 'image-webpack-loader',
             options: {
               mozjpeg: {
                 progressive: true,
-                quality: 65,
               },
+              // optipng.enabled: false will disable optipng
               optipng: {
-                enabled: true,
+                enabled: false,
               },
               pngquant: {
                 quality: [0.65, 0.90],
-                speed: 4,
+                speed: 4
               },
               gifsicle: {
                 interlaced: false,
               },
-              svgo: {
-                plugins: [
-                  {
-                    removeViewBox: false,
-                  },
-                  {
-                    cleanupIDs: false,
-                  },
-                ],
-              },
-            },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
           },
         ],
       }
