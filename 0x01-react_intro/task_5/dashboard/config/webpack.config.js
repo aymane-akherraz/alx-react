1fa9f73c3de8592a2bbbf256b98b30b206abcb44
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-07-23 19:47:00 +0100

    Added some new files

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  output: {
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.css$/, 
+        use: ["style-loader", "css-loader"] 
+      },
+      { 
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          },
+        ],
+      }
+    ]
+  },
+  devServer: {
+    static: './dist',
+    open: true,
+    compress: true,
+    port: 8564,
+    hot: true,
+  },
+}
---
+++ 0x01-react_intro/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import GetNotifs from './Notifications';
+
+describe('Notifications Component', () => {
+  it('renders without crashing', () => {
+    shallow(<GetNotifs />);
+  });
+  it('renders three <li> elements', () => {
+    const wrapper = shallow(<GetNotifs />);
+    expect(wrapper.find('li')).toHaveLength(3);
+  });
+  it('renders the text "Here is the list of notifications"', () => {
+    const wrapper = shallow(<GetNotifs />);
+    expect(wrapper.text()).toContain('Here is the list of notifications');
+  });
+});
