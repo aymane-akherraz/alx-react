4206ca1df18b3dd748559fce551377394ccc5c57
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-07-23 01:38:48 +0100

    Added task_3

---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,70 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: {
+    header: path.resolve(__dirname, './modules/header/header.js'),
+    body: path.resolve(__dirname, './modules/body/body.js'),
+    footer: path.resolve(__dirname, './modules/footer/footer.js')
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "public")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack Output",
+    }),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      { 
+        test: /\.css$/, 
+        use: ["style-loader", "css-loader"] 
+      },
+      { 
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          },
+        ],
+      }
+    ]
+  },
+  optimization: {
+		splitChunks: {
+			chunks: 'all',
+		},
+	},
+  devServer: {
+    port: 8564,
+    open: true,
+  },
+};
