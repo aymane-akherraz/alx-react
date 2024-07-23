a13d3bd9f2d0fe9735a189cac3e58d0af605175e
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-07-23 20:22:27 +0100

    Modified some files

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App/App';
-import Notifications from './Notifications/Notifications';
+import GetNotifs from './Notifications/Notifications';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<div className='root-notifications'>
-			<Notifications />
-		</div>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div className="root-notifications">
+      <GetNotifs />
+    </div>
+    <App />
+  </React.StrictMode>
 );
