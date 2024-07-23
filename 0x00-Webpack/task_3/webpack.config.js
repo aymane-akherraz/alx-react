359a24ed527e319da79a52c21a8679c9c878e6b5
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-07-23 01:59:44 +0100

    Modified some files

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,34 +1,62 @@
-const path = require("path");
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
 
 module.exports = {
-  plugins: [
-		new HTMLWebpackPlugin({
-			filename: './index.html',
-		}),
-		new CleanWebpackPlugin(),
-	],
-	devtool: 'inline-source-map',
-	mode: 'development',
-	entry: {
-		header: {
-			import: './modules/header/header.js',
-			dependOn: 'shared',
-		},
-		body: {
-			import: './modules/body/body.js',
-			dependOn: 'shared',
-		},
-		footer: {
-			import: './modules/footer/footer.js',
-			dependOn: 'shared',
-		},
-		shared: 'jquery',
-	},
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: {
+    header: path.resolve(__dirname, './modules/header/header.js'),
+    body: path.resolve(__dirname, './modules/body/body.js'),
+    footer: path.resolve(__dirname, './modules/footer/footer.js')
+  },
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: '[name].bundle.js',
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "public")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: './index.html',
+    }),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      { 
+        test: /\.css$/, 
+        use: ["style-loader", "css-loader"] 
+      },
+      { 
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          },
+        ],
+      }
+    ]
   },
   optimization: {
 		splitChunks: {
@@ -37,30 +65,7 @@ module.exports = {
 	},
   devServer: {
     static: path.join(__dirname, './public'),
-    open: true,
     port: 8564,
+    open: true,
   },
-  performance: {
-		maxAssetSize: 1000000,
-	},
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-				test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-				use: [
-					{
-						loader: ['file-loader', 'image-webpack-loader'],
-						options: {
-							bypassOnDebug: true,
-							disable: true,
-						},
-					},
-				],
-			},
-    ]
-  }
 };
