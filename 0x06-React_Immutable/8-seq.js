6fb1df8917ef78dbb6009a2346ff78191945452b
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-08-14 00:10:23 +0100

    Modified some files

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -2,13 +2,11 @@ import { Seq } from 'immutable';
 
 export default function printBestStudents(grades) {
   const BestStudents = Seq(grades)
-  .filter(student => student.score >= 70)
-  .map(student => ({
+  .filter((student) => student.score >= 70)
+  .map((student) => ({
     score: student.score,
-    firstName: 
-        student.firstName[0].toUpperCase() + student.firstName.slice(1),
-    lastName:
-        student.lastName[0].toUpperCase() + student.lastName.slice(1)
+    firstName: student.firstName[0].toUpperCase() + student.firstName.slice(1),
+    lastName: student.lastName[0].toUpperCase() + student.lastName.slice(1)
   }));
   console.log(BestStudents.toJS());
 }
